dd766640ee2b5f545222de38b1d8190a9a381f21
kaiqueDev1 <kakamelo141205@gmail.com>
2025-08-14 09:00:23 -0300

    Criando método para exibição de atributos

--- src/Filme.java
+++ src/Filme.java
@@ -5,4 +5,9 @@ public class Filme {
     double avaliacao;
     int totalDeAvaliacoes;
     int duracaoEmMinutos;
+
+    void exibeFixaTecnica(){
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+    }
 }
