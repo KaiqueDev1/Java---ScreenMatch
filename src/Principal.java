59f16df64d454539b8de81a9cc239e3dcc84e142
kaiqueDev1 <kakamelo141205@gmail.com>
2025-08-14 09:12:02 -0300

    FEAT: Chamando método de fixa técnica e criando avaliações

--- src/Principal.java
+++ src/Principal.java
@@ -7,8 +7,13 @@ public class Principal {
         meuFilme.anoDeLancamento = 1970;
         meuFilme.duracaoEmMinutos = 180;
 
-        System.out.println(meuFilme.nome);
-        System.out.println(meuFilme.anoDeLancamento);
+        meuFilme.exibeFixaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+
+        System.out.println(meuFilme.somaDasAvaliacoes);
+        System.out.println(meuFilme.totalDeAvaliacoes);
 
     }
 }
